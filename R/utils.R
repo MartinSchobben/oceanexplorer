678d296e81a6ef4b94d2aad16f8232921849c9d9
MartinSchobben <martin.schobben@hotmail.com>
2022-03-24 15:51:15 +0100

    enable pre and postfix symbols in plot labels

--- R/utils.R
+++ R/utils.R
@@ -4,7 +4,7 @@
 #'
 #' @return Expression
 #' @export
-env_parm_labeller <- function(var) {
+env_parm_labeller <- function(var, prefix = character(1), postfix = character(1)) {
   # get species / parameter names
   parm <- sub("_.*$", "", var)
   if (parm %in% c("i", "p", "o", "n")) {
@@ -14,16 +14,26 @@ env_parm_labeller <- function(var) {
     index <- c(i = 2, p = 4, o = 2, n = 3)
     as.expression(
       substitute(
-        a[b]~"("*mu*"mol kg"^{"-"}*")",
-        list(a = rlang::sym(ion[[parm]]), b = index[[parm]])
+        prefix ~ a[b]~"("*mu*"mol kg"^{"-"}*")" ~ postfix,
+        list(prefix = rlang::sym(prefix), postfix = rlang::sym(postfix), a = rlang::sym(ion[[parm]]), b = index[[parm]])
       )
     )
   } else if (parm == "t") {
-    expression('Temperature ('*degree~C*')')
+    as.expression(
+      substitute(
+        prefix ~ 'Temperature ('*degree~C*')' ~ postfix,
+        list(prefix = rlang::sym(prefix), postfix = rlang::sym(postfix))
+      )
+    )
   } else if (parm == "s") {
     "Salinity"
   } else if (parm == "I") {
-    expression("Density (kg m"^{"-3"}*")")
+    as.expression(
+      substitute(
+        prefix ~ "Density (kg m"^{"-3"}*")" ~ postfix,
+        list(prefix = rlang::sym(prefix), postfix = rlang::sym(postfix))
+      )
+    )
   } else {
     stop("Parameter is unkown.", call. = FALSE)
   }
