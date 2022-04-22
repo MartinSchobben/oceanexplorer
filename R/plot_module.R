ba5b73d1d3d10a86630be4579b747fad5f44b1a6
MartinSchobben <martin.schobben@hotmail.com>
2022-04-22 16:28:32 +0200

    remove message

--- R/plot_module.R
+++ R/plot_module.R
@@ -46,7 +46,6 @@ plot_server <- function(id, NOAA, points) {
     selected <- reactiveValues(lon = NULL, lat = NULL, depth = NULL)
 
     # plot
-    observe(message(glue::glue("{paste(input$epsg)}")))
     output$plot <- renderPlot({
 
       req(NOAA)
