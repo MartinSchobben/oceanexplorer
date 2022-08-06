5c6d8ddbe80fca0f0e71dac5a471bc3483151216
MartinSchobben <martin.schobben@hotmail.com>
2022-08-06 11:25:06 +0200

    skip spelling on CI and covr

--- tests/spelling.R
+++ tests/spelling.R
@@ -1,3 +1,5 @@
+skip_on_covr()
+skip_on_ci()
 if(requireNamespace('spelling', quietly = TRUE))
   spelling::spell_check_test(vignettes = TRUE, error = FALSE,
                              skip_on_cran = TRUE)
